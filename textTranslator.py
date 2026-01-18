0cd1493b0f737e6e1d129e1fb1acd7f56f9a0ac4
SAPCEIO <104952391+thespaceio@users.noreply.github.com>
2026-01-18 06:39:37 +0100

    Add files via upload

---
+++ textTranslator.py
@@ -0,0 +1,23 @@
+
+"""
+The following program will require internet connection to run
+"""
+import goslate
+import tkinter as tk
+from tkinter import *
+
+
+text = input("Please, input the text you want to translate: ")
+language = str(input("Please, input the short code of the language you want to translate to: "))
+text1 = goslate.Goslate()
+trans_text = text1.translate(text, language)
+
+print(trans_text)
+
+# GUI
+gui = tk.Tk()
+gui.title("Text Translator")
+gui.config(bg="black")
+
+
+gui.mainloop()
