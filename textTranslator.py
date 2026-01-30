43ac66f0eb855bbad5aca8e2424ba40f9a7e7054
SAPCEIO <104952391+thespaceio@users.noreply.github.com>
2026-01-30 10:17:15 +0100

    Update textTranslator.py for clearer language input
    Added a prompt example for language code input.

--- textTranslator.py
+++ textTranslator.py
@@ -8,7 +8,7 @@ from tkinter import *
 
 
 text = input("Please, input the text you want to translate: ")
-language = str(input("Please, input the short code of the language you want to translate to: "))
+language = str(input("Please, input the short code of the language you want to translate to(e.g EN): "))
 text1 = goslate.Goslate()
 trans_text = text1.translate(text, language)
 
@@ -18,6 +18,7 @@ print(trans_text)
 gui = tk.Tk()
 gui.title("Text Translator")
 gui.config(bg="black")
+
 
 
 gui.mainloop()
